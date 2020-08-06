8b23da4ac6f2d7b880f4f4ca39d06a5825cb1c8e
Moya Fernando <fmoya97.fm@gmail.com>
2020-08-06 18:15:01 -0300

    modal politicas de privacidad

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import Swal from 'sweetalert2';
+
+declare let $: any;
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
@@ -7,14 +10,34 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class FooterComponent implements OnInit {
-year = new Date().getFullYear();
+  year = new Date().getFullYear();
   constructor() { }
 
   ngOnInit(): void {
   }
 
   whatsApp() {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'center',
+      showConfirmButton: true,
+      customClass: { confirmButton: 'back9' }
+    });
 
+    Toast.fire({
+      title: '555 555 555',
+      background: 'rgb(233,233,0)'
+    });
+  }
+
+  privacidad(){
+    $('#privacidad').modal();
+  }
+
+  salir() {
+    setTimeout(() => {
+      $('#privacidad').modal('hide');
+    }, 300);
   }
 
 }
